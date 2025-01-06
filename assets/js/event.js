626db6ab5a9d9f78b490ef173c0cdb043e8b1b9f
MateusDaroz <matlicoop@gmail.com>
2025-01-06 16:54:47 -0300

    add: data-value to addons values for better security

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,45 +1,46 @@
-    const defaultValue = 28.99;
+const defaultValue = 28.99;
 
-    function updateTotal() {
-        let total = defaultValue;
+function updateTotal() {
+    let total = defaultValue;
 
-        document.querySelectorAll('.addon').forEach((addon) => {
-            const amount = parseInt(addon.querySelector('input[type="number"]').value);
-            const value = parseFloat(addon.querySelector('span').innerHTML);
-            total += amount * value;
-        });
-        
-        final_value = total*parseInt(document.getElementById("product-amount").value);
-        document.getElementById("final-value").innerHTML = final_value.toFixed(2);
+    document.querySelectorAll('.addon').forEach((addon) => {
+        const amount = parseInt(addon.querySelector('input[type="number"]').value);
+        const value = parseFloat(addon.querySelector('span').dataset.value);
+        total += amount * value;
+    });
+
+    final_value = total * parseInt(document.getElementById("product-amount").value);
+    document.getElementById("final-value").dataset.value = final_value.toFixed(2);
+    document.getElementById("final-value").textContent = final_value.toFixed(2);
+}
+
+function increaseAmount(button) {
+    const input = button.previousElementSibling;
+    let currentValue = parseInt(input.value);
+
+    if (currentValue < 10) {
+        input.value = currentValue + 1;
+        updateTotal();
     }
+}
 
-    function increaseAmount(button) {
-        const input = button.previousElementSibling;
-        let currentValue = parseInt(input.value);
+function decreaseAmount(button) {
+    const input = button.nextElementSibling;
+    let currentValue = parseInt(input.value);
 
-        if (currentValue < 10) {
-            input.value = currentValue + 1;
-            updateTotal();
-        }
+    if (currentValue) {
+        input.value = currentValue - 1;
+        updateTotal();
     }
+}
 
-    function decreaseAmount(button) {
-        const input = button.nextElementSibling;
-        let currentValue = parseInt(input.value);
+function decreaseQuantity(button) {
+    const input = button.nextElementSibling;
+    let currentValue = parseInt(input.value);
 
-        if (currentValue) {
-            input.value = currentValue - 1;
-            updateTotal();
-        }
+    if (currentValue > 1) {
+        input.value = currentValue - 1;
+        updateTotal();
     }
+}
 
-    function decreaseQuantity(button) {
-        const input = button.nextElementSibling;
-        let currentValue = parseInt(input.value);
-
-        if (currentValue > 1) {
-            input.value = currentValue - 1;
-            updateTotal();
-        }
-    }
-    
